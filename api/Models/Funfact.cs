109e9e1e9a3f50277555ba51ad3fffa4cb0cbaed
FelixAL07 <lerdahl07@gmail.com>
2025-08-29 13:12:09 +0200

    Models

---
+++ api/Models/Funfact.cs
@@ -0,0 +1,12 @@
+namespace api.Models
+{
+    public class Funfact
+    {
+        public string? Id { get; set; }
+        public string? Text { get; set; }
+        public string? Source { get; set; }
+        public string? SourceUrl { get; set; }
+        public string? Language { get; set; }
+        public string? Permalink { get; set; }
+    }
+}
---
+++ api/Models/Quote.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class QuoteModel
+    {
+        public string? Quote { get; set; }
+        public string? Author { get; set; }
+        public string? Category { get; set; }
+    }
+}
