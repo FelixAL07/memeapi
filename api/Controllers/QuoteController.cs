0caf866700353670747f3cf2efc6ce91e73da053
FelixAL07 <lerdahl07@gmail.com>
2025-08-29 10:20:14 +0200

    Added anpoint for quotes

---
+++ api/Controllers/QuoteController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using dotenv.net;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class QuoteController : ControllerBase
+    {
+        private static readonly HttpClient httpClient = new();
+
+
+        [HttpGet("quote")]
+        public async Task<IActionResult> GetQuote()
+        {
+            DotEnv.Load();
+
+            string? apiKey = Environment.GetEnvironmentVariable("NINJA_API_KEY");
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                return BadRequest("NINJA_API_KEY environment variable is not set.");
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.api-ninjas.com/v1/quotes");
+            request.Headers.Add("X-Api-Key", apiKey);
+            var response = await httpClient.SendAsync(request);
+            Console.WriteLine(response);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return Content(responseContent, "application/json");
+        }
+    }
+}
