85f36ae2f36b093d8eba5535138ce1b39ee74317
FelixAL07 <lerdahl07@gmail.com>
2025-09-09 14:48:57 +0200

    Changed api for quote

--- Controllers/QuoteController.cs
+++ Controllers/QuoteController.cs
@@ -17,14 +17,7 @@ namespace api.Controllers
         {
             DotEnv.Load();
 
-            string? apiKey = Environment.GetEnvironmentVariable("NINJA_API_KEY");
-            if (string.IsNullOrEmpty(apiKey))
-            {
-                return BadRequest("NINJA_API_KEY environment variable is not set.");
-            }
-
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.api-ninjas.com/v1/quotes");
-            request.Headers.Add("X-Api-Key", apiKey);
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.quotable.io/quotes/random?limit=1");
 
             var response = await httpClient.SendAsync(request);
             var responseContent = await response.Content.ReadAsStringAsync();
